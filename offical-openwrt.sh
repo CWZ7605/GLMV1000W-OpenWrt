60f44fcda3e22b790c778242ca6a9eb4ca8c8c8d
XMapst <xmapst@gmail.com>
2021-01-20 20:56:26 +0800

    update

--- offical-openwrt.sh
+++ offical-openwrt.sh
@@ -11,13 +11,27 @@
 # Add project-openwrt Lean's packages to ./package/lean.
 pushd package/
 svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lean
-svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lienol
+#svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lienol
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lienol/redsocks2 ../redsocks2
 rm -fr package/lean/luci-app-docker
+rm -fr package/lean/luci-app-ssr-plus
+popd
+
+pushd package/lean
+# Add luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld
+rm -rf helloworld/luci-app-ssr-plus/po/zh_Hans
 popd
 
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add OpenClash.
 git clone -b master --depth=1 https://github.com/vernesong/OpenClash
