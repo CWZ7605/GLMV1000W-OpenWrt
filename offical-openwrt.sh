0c69418b1bca54a17a8d979f5a5a09a9733a6739
xmapst <xmapst@gmail.com>
2021-01-20 10:22:02 +0800

    update

--- offical-openwrt.sh
+++ offical-openwrt.sh
@@ -8,34 +8,17 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Clone Lean's latest sources.
-pushd package
-git clone --depth=1 https://github.com/coolsnowwolf/lede
+# Add project-openwrt Lean's packages to ./package/lean.
+pushd package/
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lean
+rm -fr package/lean/luci-app-docker
+rm -fr package/lean/luci-app-ssr-plus
 popd
 
-# Copy Lean's packages to ./package/lean.
-mkdir package/lean
-pushd package/lede/package/lean
-cp -r {adbyby,automount,baidupcs-web,coremark,ddns-scripts_aliyun,ddns-scripts_dnspod,dns2socks,ipt2socks,ipv6-helper,kcptun,luci-app-adbyby-plus,luci-app-arpbind,luci-app-autoreboot,luci-app-baidupcs-web,luci-app-cifs-mount,luci-app-cpufreq,luci-app-familycloud,luci-app-filetransfer,luci-app-frpc,luci-app-n2n_v2,luci-app-netdata,luci-app-nfs,luci-app-nft-qos,luci-app-nps,luci-app-ps3netsrv,luci-app-softethervpn,luci-app-usb-printer,luci-app-unblockmusic,luci-app-verysync,luci-app-vsftpd,luci-app-webadmin,luci-app-xlnetacc,luci-lib-fs,microsocks,n2n_v2,npc,pdnsd-alt,proxychains-ng,ps3netsrv,redsocks2,shadowsocksr-libev,simple-obfs,softethervpn5,srelay,tcpping,trojan,UnblockNeteaseMusic,UnblockNeteaseMusicGo,uugamebooster,v2ray,v2ray-plugin,verysync,vsftpd-alt,xray} "../../../lean"
-popd
-
-# Default settings
 pushd package/lean
-git clone --depth=1 https://github.com/SuLingGG/default-settings
-
-# Add Project OpenWrt's autocore
-rm -rf autocore
-svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/autocore
-
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
 rm -rf helloworld/luci-app-ssr-plus/po/zh_Hans
-
-popd
-
-# Clean Lean's code
-pushd package
-rm -rf lede
 popd
 
 # Clone community packages to package/community
@@ -61,6 +44,11 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/AdGuardHo
 # Add smartdns
 svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
+
+# Add redsocks2
+git clone https://github.com/hqvv/luci-app-redsocks2.git
+mv luci-app-redsocks2/redsocks2 ../
+rm -fr luci-app-redsocks2
 
 # Add luci-udptools
 git clone --depth=1 https://github.com/zcy85611/openwrt-luci-kcp-udp
@@ -116,3 +104,7 @@ popd
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
+# Convert Translation
+cp ../convert-translation.sh .
+chmod +x ./convert-translation.sh
+./convert-translation.sh || true
